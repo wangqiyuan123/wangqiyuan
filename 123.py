35d2d3d80117256c6da776a04abecad0de9d6778
wangqiyuan123 <3190514614@qq.com>
2023-11-01 09:24:29 +0800

    Signed-off-by: wangqiyuan123 <3190514614@qq.com

--- 123.py
+++ 123.py
@@ -1,15 +1,16 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-# from selenium.common.exceptions import NoSuchElementException
-wd = webdriver.Chrome()
-wd.get('https://cdn2.byhy.net/files/selenium/sample1.html')
-
-# elements = wd.find_elements(By.TAG_NAME,"span")
-# elements.send_keys("通讯")
-elements=wd.find_elements(By.ID,"container")
-# element=wd.find_element(By.ID,"go")
-# element.click()
-
-for element in elements:
-    print(element.text)
-input()
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# # from selenium.common.exceptions import NoSuchElementException
+# wd = webdriver.Chrome()
+# wd.get('https://cdn2.byhy.net/files/selenium/sample1.html')
+#
+# # elements = wd.find_elements(By.TAG_NAME,"span")
+# # elements.send_keys("通讯")
+# elements=wd.find_elements(By.ID,"container")
+# # element=wd.find_element(By.ID,"go")
+# # element.click()
+#
+# for element in elements:
+#     print(element.text)
+# input()
+print("12334")
